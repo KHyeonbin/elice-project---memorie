fb44b1399da4ca1b728ae678151c6cae0fc82c65
bini <dovesks@naver.com>
2024-06-17 20:37:02 +0900

    mainpagelist

--- src/routers/views-router.js
+++ src/routers/views-router.js
@@ -14,6 +14,7 @@ viewsRouter.use('/admin', serveStatic('admin'));
 viewsRouter.use('/productadd', serveStatic('productadd'));
 viewsRouter.use('/product-manage', serveStatic('product-manage'));
 viewsRouter.use('/productlist', serveStatic('productlist'));
+viewsRouter.use('/single-item', serveStatic('single-item'));
 
 // views 폴더의 최상단 파일인 rabbit.png, api.js 등을 쓸 수 있게 함
 viewsRouter.use('/', serveStatic(''));
