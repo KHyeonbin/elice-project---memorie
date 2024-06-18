242715d7b804db7e86e5dd3be1cfab1ab5ec5b1e
jung su yeon <02tndus@gmail.com>
2024-06-18 20:35:20 +0900

    feat:bodycare, handcare, homefragrance, perfume 완성

--- src/routers/views-router.js
+++ src/routers/views-router.js
@@ -18,7 +18,10 @@ viewsRouter.use('/single-item', serveStatic('single-item'));
 viewsRouter.use('/about', serveStatic('about'));
 viewsRouter.use('/userlists', serveStatic('userlists'));
 viewsRouter.use('/orderlist', serveStatic('orderlist'));
-
+viewsRouter.use('/homefragrance', serveStatic('homefragrance'));
+viewsRouter.use('/handcare', serveStatic('handcare'));
+viewsRouter.use('/bodycare', serveStatic('bodycare'));
+viewsRouter.use('/perfume', serveStatic('perfume'));
 // views 폴더의 최상단 파일인 rabbit.png, api.js 등을 쓸 수 있게 함
 viewsRouter.use('/', serveStatic(''));
 
